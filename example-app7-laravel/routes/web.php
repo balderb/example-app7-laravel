88e7d082f68260e5239313f05aea0f0dfe4d0bd7
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2022-02-19 11:15:31 +0100

    init testing

--- example-app7-laravel/routes/web.php
+++ example-app7-laravel/routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/hello', function () {
     return view('welcome');
+});
+
+Route::get('/testing1', function () {
+    return view('testing1');
+});
+
+Route::get('/testing2', function () {
+    return view('testing2');
 });
